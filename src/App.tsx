7240068d9ad06ad5074c4efdd547c5249d4462fa
goku1369 <142935847+goku1369@users.noreply.github.com>
2023-12-17 11:35:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-// import functionalities
 import React from 'react';
-import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
   PublicKey,
+  SystemProgram,
   Transaction,
+  clusterApiUrl,
+  sendAndConfirmTransaction,
 } from "@solana/web3.js";
-import {useEffect , useState } from "react";
-import './App.css'
+import { useEffect, useState } from "react";
+import * as buffer from "buffer";
+window.Buffer = buffer.Buffer;
 
-// create types
 type DisplayEncoding = "utf8" | "hex";
-
 type PhantomEvent = "disconnect" | "connect" | "accountChanged";
 type PhantomRequestMethod =
   | "connect"
@@ -23,7 +28,6 @@ interface ConnectOpts {
   onlyIfTrusted: boolean;
 }
 
-// create a provider interface (hint: think of this as an object) to store the Phantom Provider
 interface PhantomProvider {
   publicKey: PublicKey | null;
   isConnected: boolean | null;
@@ -39,121 +43,235 @@ interface PhantomProvider {
   request: (method: PhantomRequestMethod, params: any) => Promise<unknown>;
 }
 
- /**
- * @description gets Phantom provider, if it exists
- */
- const getProvider = (): PhantomProvider | undefined => {
+const getProvider = (): PhantomProvider | undefined => {
   if ("solana" in window) {
-    // @ts-ignore
     const provider = window.solana as any;
     if (provider.isPhantom) return provider as PhantomProvider;
   }
 };
 
 export default function App() {
-  // create state variable for the provider
   const [provider, setProvider] = useState<PhantomProvider | undefined>(
     undefined
   );
 
-	// create state variable for the wallet key
-  const [walletKey, setWalletKey] = useState<String | undefined>(
-  undefined
+  const [receiverPublicKey, setReceiverPublicKey] = useState<PublicKey | undefined>(
+    undefined
   );
 
-  // this is the function that runs whenever the component updates (e.g. render, refresh)
-  useEffect(() => {
-	  const provider = getProvider();
+  const [senderKeypair, setSenderKeypair] = useState<Keypair | undefined>(
+    undefined
+  );
 
-		// if the phantom provider exists, set this as the provider
-	  if (provider) setProvider(provider);
-	  else setProvider(undefined);
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+  useEffect(() => {
+    const provider = getProvider();
+    if (provider) setProvider(provider);
+    else setProvider(undefined);
   }, []);
 
-  /**
-   * @description prompts user to connect wallet if it exists.
-	 * This function is called when the connect wallet button is clicked
-   */
+  const showToast = (message: string, type: 'success' | 'error' | 'info') => {
+    switch (type) {
+      case 'success':
+        toast.success(message);
+        break;
+      case 'error':
+        toast.error(message);
+        break;
+      case 'info':
+        toast.info(message);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const createSender = async () => {
+    try {
+      const newPair = Keypair.generate();
+      const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+      setSenderKeypair(newPair);
+
+      const fromAirDropSignature = await connection.requestAirdrop(
+        newPair.publicKey,
+        2 * LAMPORTS_PER_SOL
+      );
+      await connection.confirmTransaction(fromAirDropSignature, "confirmed");
+
+      showToast("Airdrop successful.", 'success');
+      console.log('Wallet Balance: ' + (await connection.getBalance(newPair.publicKey)) / LAMPORTS_PER_SOL);
+    } catch (err) {
+      showToast("Airdrop error: " + (err as Error).message, 'error');
+    }
+  }
+
   const connectWallet = async () => {
-    // @ts-ignore
+      // @ts-ignore
     const { solana } = window;
-
-		// checks if phantom wallet exists
     if (solana) {
       try {
-				// connects wallet and returns response which includes the wallet public key
         const response = await solana.connect();
-        console.log('wallet account ', response.publicKey.toString());
-				// update walletKey to be the public key
-        setWalletKey(response.publicKey.toString());
+        setReceiverPublicKey(new PublicKey(response.publicKey));
+        showToast("Connected to Phantom wallet.", 'success');
       } catch (err) {
-          console.log(err);
+        showToast("Error connecting to Phantom wallet: " + (err as Error).message, 'error');
       }
     }
   };
 
-  /**
-   * @description disconnects wallet if it exists.
-	 * This function is called when the disconnect wallet button is clicked
-   */
   const disconnectWallet = async () => {
-    // @ts-ignore
+      // @ts-ignore
     const { solana } = window;
-
-		// checks if phantom wallet exists
     if (solana) {
       try {
-				// ADD DISCONNECT LOGIC HERE
+        solana.disconnect();
+        setReceiverPublicKey(undefined);
+        showToast("Wallet disconnected.", 'success');
       } catch (err) {
-          console.log(err);
+        showToast("Error disconnecting wallet: " + (err as Error).message, 'error');
       }
     }
   };
 
-	// HTML code for the app
+  const transferSol = async () => {
+    if (!receiverPublicKey) {
+      showToast("Receiver public key is undefined", 'error');
+      return;
+    }
+
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const from = Keypair.fromSecretKey(senderKeypair!.secretKey);
+
+    try {
+      const transaction = new Transaction().add(
+        SystemProgram.transfer({
+          fromPubkey: from.publicKey,
+          toPubkey: receiverPublicKey,
+          lamports: 1 * LAMPORTS_PER_SOL,
+        })
+      );
+
+      console.log("Sending transaction...");
+      const signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [from],
+        { commitment: "confirmed" }
+      );
+
+      showToast("Transaction confirmed with signature: " + signature, 'success');
+      console.log("Sender Balance:", await connection.getBalance(from.publicKey) / LAMPORTS_PER_SOL);
+      console.log("Receiver Balance:", await connection.getBalance(receiverPublicKey) / LAMPORTS_PER_SOL);
+    } catch (error) {
+      showToast("Error sending transaction: " + error, 'error');
+    }
+  };
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Connect to Phantom Wallet</h2>
-      {provider && !walletKey && (
-      <button
-        style={{
-          fontSize: "16px",
-          padding: "15px",
-          fontWeight: "bold",
-          borderRadius: "5px",
-        }}
-        onClick={connectWallet}
-      >
-        Connect Wallet
-      </button>
-        )}
-        {provider && walletKey && (
+        <h2>Module 2 Assessment</h2>
+        <span className="buttons">
+          <button
+            onClick={createSender}
+            style={{
+              fontSize: "16px",
+              padding: "15px",
+              fontWeight: "bold",
+              borderRadius: "5px",
+              margin: "5px",
+              background: "#4CAF50", /* Green */
+              color: "white",
+              border: "none",
+              textAlign: "center",
+              textDecoration: "none",
+              display: "inline-block",
+              transitionDuration: "0.4s",
+              cursor: "pointer",
+            }}
+          >
+            Create a New Solana Account
+          </button>
+          {provider && !receiverPublicKey && (
+            <button
+              onClick={connectWallet}
+              style={{
+                fontSize: "16px",
+                padding: "15px",
+                fontWeight: "bold",
+                borderRadius: "5px",
+                margin: "5px",
+                background: "#008CBA", /* Blue */
+                color: "white",
+                border: "none",
+                textAlign: "center",
+                textDecoration: "none",
+                display: "inline-block",
+                transitionDuration: "0.4s",
+                cursor: "pointer",
+              }}
+            >
+              Connect to Phantom Wallet
+            </button>
+          )}
+          {provider && receiverPublicKey && (
             <div>
-              <p>{/*FOR WALLET ADDRESS*/}</p>
               <button
+                onClick={disconnectWallet}
                 style={{
                   fontSize: "16px",
                   padding: "15px",
                   fontWeight: "bold",
                   borderRadius: "5px",
-                  position: "absolute",
-                  top: "28px",
-                  right: "28px"
+                  margin: "5px",
+                  background: "#f44336", /* Red */
+                  color: "white",
+                  border: "none",
+                  textAlign: "center",
+                  textDecoration: "none",
+                  display: "inline-block",
+                  transitionDuration: "0.4s",
+                  cursor: "pointer",
                 }}
-                onClick={disconnectWallet}
               >
-                Disconnect Wallet
+                Disconnect from Wallet
               </button>
             </div>
-        )}
+          )}
+          {provider && receiverPublicKey && senderKeypair && (
+            <button
+              onClick={transferSol}
+              style={{
+                fontSize: "16px",
+                padding: "15px",
+                fontWeight: "bold",
+                borderRadius: "5px",
+                margin: "5px",
+                background: "#4CAF50", /* Green */
+                color: "white",
+                border: "none",
+                textAlign: "center",
+                textDecoration: "none",
+                display: "inline-block",
+                transitionDuration: "0.4s",
+                cursor: "pointer",
+              }}
+            >
+              Transfer SOL to Phantom Wallet
+            </button>
+          )}
+        </span>
         {!provider && (
           <p>
-            No provider found. Install{" "}
+            No provider found. Install{' '}
             <a href="https://phantom.app/">Phantom Browser extension</a>
           </p>
         )}
-        </header>
+        <ToastContainer />
+      </header>
     </div>
   );
 }
